9ab10001d19561a32f238812b3be9c182b2757fe
Adam Portier <ajportier@gmail.com>
2014-10-07 15:23:54 -0400

    Added additional math functions to REST backend

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import (Flask, jsonify, request, abort, render_template)
+import math
 app = Flask(__name__)
 
 @app.route('/')
@@ -52,6 +53,64 @@ def divide_args():
         answer = arg1 / arg2
         return (jsonify({'answer':answer}), 200)
     except KeyError:
+        abort(400)
+    except ZeroDivisionError:
+        abort(400)
+
+@app.route('/mod', methods=['POST'])
+def mod_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = arg1 % arg2
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+    except ZeroDivisionError:
+        abort(400)
+
+@app.route('/exp', methods=['POST'])
+def exponent_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = arg1 ** arg2
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+
+@app.route('/average', methods=['POST'])
+def average_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = (arg1 + arg2) / 2
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+
+@app.route('/log', methods=['POST'])
+def log_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = math.log(arg1, arg2)
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+    except ZeroDivisionError:
+        # occurs when you try a log(a,1)
+        abort(400)
+    except ValueError:
+        # occurs when you try a log(a,0)
         abort(400)
 
 if __name__ == '__main__':
